c8d3608c5ac1da18cceb489c07c343851e21559f
Gabriel Monteiro <98609413+Gfmonteiro04@users.noreply.github.com>
2024-06-10 15:02:34 -0300

    Delete Back-end.Certibrasil/certibrasil directory

--- Back-end.Certibrasil/certibrasil/mapa/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Cliente, Endereco, Certificacao 
-
-admin.site.register(Cliente)  
-admin.site.register(Endereco) 
-admin.site.register(Certificacao)
